7dd046eb6427ac8fa08164da28b7d0aeffac4cfa
Sebastiaan Hekner <shekner@gmail.com>
2020-04-17 17:01:31 +0200

    Project lists

--- src/app.ts
+++ src/app.ts
@@ -155,6 +155,8 @@ class ProjectInput {
   }
 }
 
+
+// Runtime code
 const prjInput = new ProjectInput();
 const activePrjList = new ProjectList('active');
 const finishedPrjList = new ProjectList('finished');
